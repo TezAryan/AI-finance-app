c0b10e09c0771d1b6380cb00493126b705a9af7a
TezAryan <tezaryan2004@gmail.com>
2025-04-27 23:55:12 +0530

    first commit

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,8 +2,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const HeroSection = () => {
+
+    const imageRef = useRef();
+
+    useEffect(() => {
+        const imageElement = imageRef.current;
+        const scrollPosition = window.scrollY;
+        const scrollThreshold = 100;
+
+        window.addEventListener("scroll" , handleScroll)
+    }, [])
   return (
     <div className="pb-20 px-4">
         <div className="container mx-auto text-center">
@@ -11,7 +22,7 @@ const HeroSection = () => {
             <p  className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto" > An AI-powered financial management platform that helps you track,
                 analyze ,and optimize your spending with real-time insights.
             </p>
-            <div>
+            <div className="flex-justify-center space-x-4">
                 <Link href="/dashboard">
                     <Button size='lg' className='px-8'>
                         Get Started
@@ -22,13 +33,15 @@ const HeroSection = () => {
                     <Button size='lg' variant='outline' className='px-8'>Watch Demo</Button>
                 </Link>
             </div>
-            <div>
+        <div className="hero-image-wrapper">
+            <div ref={imageRef} className="hero-image">
                 <Image src='/banner.jpeg'
                 width={1280}
                 height={720}
                 alt="Dashboard Preview"
                 className="rounded-lg shadow-2xl border mx-auto"/>
             </div>
+         </div>
         </div>
     </div>
   )
