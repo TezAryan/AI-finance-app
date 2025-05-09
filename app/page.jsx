676161147f078a3e849c87f15eecebf1de64d508
TezAryan <tezaryan2004@gmail.com>
2025-05-09 22:46:08 +0530

    normal progress

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { howItWorksData, statsData, testimonialsData } from "@/data/landing";
 import Image from "next/image";
 import { featuresData } from "@/data/landing";
 import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -18,7 +19,7 @@ export default function Home() {
                 <div className="text-4xl font-bold text-blue-600 mb-2">{statsData.value}</div>
                 <div className="text-gray-600">{statsData.label}</div>
               </div>
-            ))}
+            ))} 
           </div>
         </div>
       </section>
@@ -70,13 +71,19 @@ export default function Home() {
             {testimonialsData.map((testimonial, index) => (
               <Card key={index} className="p-6">
                 <CardContent className=" pt-4">
-                <div>
+                <div className="flex items-center mb-4">
                   <Image src={testimonial.image}
                   alt={testimonial.name}
                   width={40}
                   height={40}
                   className="rounded-full"/>
+
+                  <div className="ml-4">
+                    <div className="font-semibold"> {testimonial.name}</div>
+                    <div className="text-sm text-gray-600">{testimonial.role}</div>
+                  </div>
                 </div>
+                <p className="text-gray-600">{testimonial.quote}</p>
                 </CardContent>
 
               </Card>
@@ -84,6 +91,25 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className="py-20 bg-blue-600">
+        <div className="container mx-auto px-4 text-center">
+            <h2 className=" text-3xl font-bold text-center mb-4">
+                 Ready to take Control of Your Finances?  
+            </h2>
+            <p className="text-blue-100 mb-8 max-w-2xl mx-auto">
+              Join thousands of users who are already managing their finances
+              smarter with Welth
+            </p>
+            <Link href="/dashboard">
+            <Button size="lg"
+            className="bg-white text-blue-600 hover: bg-blue-50 animate-bounce">
+            Start Free Trail
+            </Button>
+          
+            </Link>
+          </div> 
+          </section>
 
 
     </div>
